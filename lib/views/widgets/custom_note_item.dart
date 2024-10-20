d4880cf15e202ae1484ebe982f7279ed435218f5
mohamedjamaal <mohamed_gamal19@hotmail.com>
2024-10-20 12:53:45 +0300

    Add theme and card item ui

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NotesItem extends StatelessWidget {
+  const NotesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      decoration: BoxDecoration(
+        color: const Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter Tips',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26,
+              ),
+            ),
+            subtitle: const Padding(
+              padding: EdgeInsets.only(top: 16, bottom: 16),
+              child: Text(
+                'Build your career with mohamed gamal',
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 24,
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(right: 32),
+            child: Text('20-10-2024',
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 16,
+                )),
+          ),
+        ],
+      ),
+    );
+  }
+}
