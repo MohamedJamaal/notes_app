5660ff389b29115670492717b5557440ff9c3936
mohamedjamaal <mohamed_gamal19@hotmail.com>
2024-10-21 10:42:37 +0300

    add bottom sheet button and text fields

---
+++ lib/views/widgets/custom_text_filed.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+class CustomTextFiled extends StatelessWidget {
+  const CustomTextFiled({super.key, required this.hint, this.maxLines = 1});
+
+  final String hint;
+  final int maxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: KPrimaryColor,
+      maxLength: maxLines,
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: TextStyle(color: KPrimaryColor),
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(KPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(
+        color: color ?? Colors.white,
+      ),
+    );
+  }
+}
