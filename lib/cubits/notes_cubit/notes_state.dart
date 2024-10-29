84e18f072771a4e4e5c620eac101f87b7baf2de9
mohamedjamaal <mohamed_gamal19@hotmail.com>
2024-10-29 12:54:11 +0300

    refactor for part one

--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -1,6 +1,6 @@
 part of 'notes_cubit.dart';
 
 @immutable
-sealed class NotesState {}
+abstract class NotesState {}
 
-final class NotesInitial extends NotesState {}
+class NotesInitial extends NotesState {}
