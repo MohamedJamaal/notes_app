113d1f5a1a8d5575c910060b68da2aaff09baadc
mohamedjamaal <mohamed_gamal19@hotmail.com>
2024-11-03 15:07:34 +0200

    add edit color in edit note page & finish app

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,10 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff62FCD7);
 const kNotesBox = 'notes_box';
+List<Color> KColors = const [
+  Color(0xff0D1F2D),
+  Color(0xff546A7B),
+  Color(0xff9EA3B0),
+  Color(0xffFAE1DF),
+  Color(0xffE4C3AD),
+];
---
+++ lib/views/widgets/edit_note_colors_list_view.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/views/widgets/colors_list_view.dart';
+
+class EditNoteColorsList extends StatefulWidget {
+  const EditNoteColorsList({super.key, required this.note});
+  final NoteModel note;
+  @override
+  State<EditNoteColorsList> createState() => _EditNoteColorsListState();
+}
+
+class _EditNoteColorsListState extends State<EditNoteColorsList> {
+  late int currentIndex;
+  @override
+  void initState() {
+    // make inital color is the color of the note
+    currentIndex = KColors.indexOf(Color(widget.note.color));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 38 * 2,
+      child: ListView.builder(
+        itemCount: KColors.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                widget.note.color = KColors[index].value;
+                setState(() {});
+              },
+              child: ColorItem(
+                isActive: currentIndex == index,
+                color: KColors[index],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
